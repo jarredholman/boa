06ec632fb48d58d373b4b93b6d0cd3c06e157ea9
HalidOdat <halidodat@gmail.com>
2020-08-08 11:58:54 +0200

    Made iterator names more meaningful

--- boa/src/builtins/object/iter.rs
+++ boa/src/builtins/object/iter.rs
@@ -23,48 +23,48 @@ impl Object {
     }
 
     #[inline]
-    pub fn symbols(&self) -> Symbols<'_> {
-        Symbols(self.symbol_properties.iter())
+    pub fn symbol_properties(&self) -> SymbolProperties<'_> {
+        SymbolProperties(self.symbol_properties.iter())
     }
 
     #[inline]
-    pub fn symbol_keys(&self) -> SymbolKeys<'_> {
-        SymbolKeys(self.symbol_properties.keys())
+    pub fn symbol_property_keys(&self) -> SymbolPropertyKeys<'_> {
+        SymbolPropertyKeys(self.symbol_properties.keys())
     }
 
     #[inline]
-    pub fn symbol_values(&self) -> SymbolValues<'_> {
-        SymbolValues(self.symbol_properties.values())
+    pub fn symbol_property_values(&self) -> SymbolPropertyValues<'_> {
+        SymbolPropertyValues(self.symbol_properties.values())
     }
 
     #[inline]
-    pub fn indexes(&self) -> Indexes<'_> {
-        Indexes(self.indexed_properties.iter())
+    pub fn index_properties(&self) -> IndexProperties<'_> {
+        IndexProperties(self.indexed_properties.iter())
     }
 
     #[inline]
-    pub fn index_keys(&self) -> IndexKeys<'_> {
-        IndexKeys(self.indexed_properties.keys())
+    pub fn index_property_keys(&self) -> IndexPropertyKeys<'_> {
+        IndexPropertyKeys(self.indexed_properties.keys())
     }
 
     #[inline]
-    pub fn index_values(&self) -> IndexValues<'_> {
-        IndexValues(self.indexed_properties.values())
+    pub fn index_property_values(&self) -> IndexPropertyValues<'_> {
+        IndexPropertyValues(self.indexed_properties.values())
     }
 
     #[inline]
-    pub fn strings(&self) -> Strings<'_> {
-        Strings(self.properties.iter())
+    pub fn string_properties(&self) -> StringProperties<'_> {
+        StringProperties(self.properties.iter())
     }
 
     #[inline]
-    pub fn string_keys(&self) -> StringKeys<'_> {
-        StringKeys(self.properties.keys())
+    pub fn string_property_keys(&self) -> StringPropertyKeys<'_> {
+        StringPropertyKeys(self.properties.keys())
     }
 
     #[inline]
-    pub fn string_values(&self) -> StringValues<'_> {
-        StringValues(self.properties.values())
+    pub fn string_property_values(&self) -> StringPropertyValues<'_> {
+        StringPropertyValues(self.properties.values())
     }
 }
 
@@ -139,9 +139,9 @@ impl ExactSizeIterator for Values<'_> {
 impl FusedIterator for Values<'_> {}
 
 #[derive(Debug, Clone)]
-pub struct Symbols<'a>(hash_map::Iter<'a, RcSymbol, Property>);
+pub struct SymbolProperties<'a>(hash_map::Iter<'a, RcSymbol, Property>);
 
-impl<'a> Iterator for Symbols<'a> {
+impl<'a> Iterator for SymbolProperties<'a> {
     type Item = (&'a RcSymbol, &'a Property);
 
     #[inline]
@@ -155,19 +155,19 @@ impl<'a> Iterator for Symbols<'a> {
     }
 }
 
-impl ExactSizeIterator for Symbols<'_> {
+impl ExactSizeIterator for SymbolProperties<'_> {
     #[inline]
     fn len(&self) -> usize {
         self.0.len()
     }
 }
 
-impl FusedIterator for Symbols<'_> {}
+impl FusedIterator for SymbolProperties<'_> {}
 
 #[derive(Debug, Clone)]
-pub struct SymbolKeys<'a>(hash_map::Keys<'a, RcSymbol, Property>);
+pub struct SymbolPropertyKeys<'a>(hash_map::Keys<'a, RcSymbol, Property>);
 
-impl<'a> Iterator for SymbolKeys<'a> {
+impl<'a> Iterator for SymbolPropertyKeys<'a> {
     type Item = &'a RcSymbol;
 
     #[inline]
@@ -181,19 +181,19 @@ impl<'a> Iterator for SymbolKeys<'a> {
     }
 }
 
-impl ExactSizeIterator for SymbolKeys<'_> {
+impl ExactSizeIterator for SymbolPropertyKeys<'_> {
     #[inline]
     fn len(&self) -> usize {
         self.0.len()
     }
 }
 
-impl FusedIterator for SymbolKeys<'_> {}
+impl FusedIterator for SymbolPropertyKeys<'_> {}
 
 #[derive(Debug, Clone)]
-pub struct SymbolValues<'a>(hash_map::Values<'a, RcSymbol, Property>);
+pub struct SymbolPropertyValues<'a>(hash_map::Values<'a, RcSymbol, Property>);
 
-impl<'a> Iterator for SymbolValues<'a> {
+impl<'a> Iterator for SymbolPropertyValues<'a> {
     type Item = &'a Property;
 
     #[inline]
@@ -207,19 +207,19 @@ impl<'a> Iterator for SymbolValues<'a> {
     }
 }
 
-impl ExactSizeIterator for SymbolValues<'_> {
+impl ExactSizeIterator for SymbolPropertyValues<'_> {
     #[inline]
     fn len(&self) -> usize {
         self.0.len()
     }
 }
 
-impl FusedIterator for SymbolValues<'_> {}
+impl FusedIterator for SymbolPropertyValues<'_> {}
 
 #[derive(Debug, Clone)]
-pub struct Indexes<'a>(hash_map::Iter<'a, u32, Property>);
+pub struct IndexProperties<'a>(hash_map::Iter<'a, u32, Property>);
 
-impl<'a> Iterator for Indexes<'a> {
+impl<'a> Iterator for IndexProperties<'a> {
     type Item = (&'a u32, &'a Property);
 
     #[inline]
@@ -233,19 +233,19 @@ impl<'a> Iterator for Indexes<'a> {
     }
 }
 
-impl ExactSizeIterator for Indexes<'_> {
+impl ExactSizeIterator for IndexProperties<'_> {
     #[inline]
     fn len(&self) -> usize {
         self.0.len()
     }
 }
 
-impl FusedIterator for Indexes<'_> {}
+impl FusedIterator for IndexProperties<'_> {}
 
 #[derive(Debug, Clone)]
-pub struct IndexKeys<'a>(hash_map::Keys<'a, u32, Property>);
+pub struct IndexPropertyKeys<'a>(hash_map::Keys<'a, u32, Property>);
 
-impl<'a> Iterator for IndexKeys<'a> {
+impl<'a> Iterator for IndexPropertyKeys<'a> {
     type Item = &'a u32;
 
     #[inline]
@@ -259,19 +259,19 @@ impl<'a> Iterator for IndexKeys<'a> {
     }
 }
 
-impl ExactSizeIterator for IndexKeys<'_> {
+impl ExactSizeIterator for IndexPropertyKeys<'_> {
     #[inline]
     fn len(&self) -> usize {
         self.0.len()
     }
 }
 
-impl FusedIterator for IndexKeys<'_> {}
+impl FusedIterator for IndexPropertyKeys<'_> {}
 
 #[derive(Debug, Clone)]
-pub struct IndexValues<'a>(hash_map::Values<'a, u32, Property>);
+pub struct IndexPropertyValues<'a>(hash_map::Values<'a, u32, Property>);
 
-impl<'a> Iterator for IndexValues<'a> {
+impl<'a> Iterator for IndexPropertyValues<'a> {
     type Item = &'a Property;
 
     #[inline]
@@ -285,19 +285,19 @@ impl<'a> Iterator for IndexValues<'a> {
     }
 }
 
-impl ExactSizeIterator for IndexValues<'_> {
+impl ExactSizeIterator for IndexPropertyValues<'_> {
     #[inline]
     fn len(&self) -> usize {
         self.0.len()
     }
 }
 
-impl FusedIterator for IndexValues<'_> {}
+impl FusedIterator for IndexPropertyValues<'_> {}
 
 #[derive(Debug, Clone)]
-pub struct Strings<'a>(hash_map::Iter<'a, RcString, Property>);
+pub struct StringProperties<'a>(hash_map::Iter<'a, RcString, Property>);
 
-impl<'a> Iterator for Strings<'a> {
+impl<'a> Iterator for StringProperties<'a> {
     type Item = (&'a RcString, &'a Property);
 
     #[inline]
@@ -311,19 +311,19 @@ impl<'a> Iterator for Strings<'a> {
     }
 }
 
-impl ExactSizeIterator for Strings<'_> {
+impl ExactSizeIterator for StringProperties<'_> {
     #[inline]
     fn len(&self) -> usize {
         self.0.len()
     }
 }
 
-impl FusedIterator for Strings<'_> {}
+impl FusedIterator for StringProperties<'_> {}
 
 #[derive(Debug, Clone)]
-pub struct StringKeys<'a>(hash_map::Keys<'a, RcString, Property>);
+pub struct StringPropertyKeys<'a>(hash_map::Keys<'a, RcString, Property>);
 
-impl<'a> Iterator for StringKeys<'a> {
+impl<'a> Iterator for StringPropertyKeys<'a> {
     type Item = &'a RcString;
 
     #[inline]
@@ -337,19 +337,19 @@ impl<'a> Iterator for StringKeys<'a> {
     }
 }
 
-impl ExactSizeIterator for StringKeys<'_> {
+impl ExactSizeIterator for StringPropertyKeys<'_> {
     #[inline]
     fn len(&self) -> usize {
         self.0.len()
     }
 }
 
-impl FusedIterator for StringKeys<'_> {}
+impl FusedIterator for StringPropertyKeys<'_> {}
 
 #[derive(Debug, Clone)]
-pub struct StringValues<'a>(hash_map::Values<'a, RcString, Property>);
+pub struct StringPropertyValues<'a>(hash_map::Values<'a, RcString, Property>);
 
-impl<'a> Iterator for StringValues<'a> {
+impl<'a> Iterator for StringPropertyValues<'a> {
     type Item = &'a Property;
 
     #[inline]
@@ -363,11 +363,11 @@ impl<'a> Iterator for StringValues<'a> {
     }
 }
 
-impl ExactSizeIterator for StringValues<'_> {
+impl ExactSizeIterator for StringPropertyValues<'_> {
     #[inline]
     fn len(&self) -> usize {
         self.0.len()
     }
 }
 
-impl FusedIterator for StringValues<'_> {}
+impl FusedIterator for StringPropertyValues<'_> {}
